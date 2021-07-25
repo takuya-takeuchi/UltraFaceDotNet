7e3c73a02c3c1f4bd93ff6e2abd825d0d50e518b
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-07-25 17:03:28 +0900

    fix

---
+++ nuget/CreateAllPackage.ps1
@@ -0,0 +1,18 @@
+$targets = @(
+   "CPU",
+   "GPU"
+)
+
+$ScriptPath = $PSScriptRoot
+$UltraFaceDotNetRoot = Split-Path $ScriptPath -Parent
+
+$source = Join-Path $UltraFaceDotNetRoot src | `
+          Join-Path -ChildPath UltraFaceDotNet
+dotnet restore ${source}
+# build for general
+dotnet build -c Release ${source} /nowarn:CS1591
+
+foreach ($target in $targets)
+{
+   pwsh CreatePackage.ps1 $target
+}
